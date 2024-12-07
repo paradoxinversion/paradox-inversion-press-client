d862efbf3539bc781d001b9f2ec60bf3409729c1
Jedai Saboteur <paradox.inversion@gmail.com>
2024-12-07 13:44:48 -0700

    refactor: add metadata to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Paradox Inversion Press",
+  description:
+    "Paradox Inversion Press is the home of creative works by Jedai Saboteur",
+};
 
 export default function RootLayout({
   children,
