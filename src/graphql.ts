eaf1f5b164b3817be23b148cdb2e903d96c77c9d
Jedai Saboteur <paradox.inversion@gmail.com>
2025-05-31 14:54:38 -0600

    refactor: simplify fetchGraphQL function by consolidating API_URI and removing buildQuery logic

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
 
-// let API_URI = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/graphql";
+const API_URI = process.env.API_URL || "http://localhost:3000/api/graphql";
 
 export const gql = ([content]) => content;
 
 export async function fetchGraphQL(query, variables?, buildQuery = false) {
-  let apiResponse;
-  if (buildQuery) {
-    apiResponse = await axios.post(process.env.API_URL!, { query, variables });
-  } else {
-    apiResponse = await axios.post(process.env.NEXT_PUBLIC_API_URL!, { query, variables });
-  }
+  console.log("buildQuery:", buildQuery);
+  const apiResponse = await axios.post(API_URI, { query, variables });
+  // if (buildQuery) {
+  //   console.log("Fetching GraphQL with buildQuery enabled:", process.env.API_URL);
+  //   apiResponse = await axios.post(process.env.API_URL!, { query, variables });
+  // } else {
+  //   apiResponse = await axios.post(process.env.NEXT_PUBLIC_API_URL!, { query, variables });
+  // }
+
   return apiResponse.data.data;
 }
