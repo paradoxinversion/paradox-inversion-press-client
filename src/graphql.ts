be3d2530592e05d50fbf1acaa46c202784ab02ab
Jedai Saboteur <paradox.inversion@gmail.com>
2025-05-31 15:43:30 -0600

    refactor: uncomment and clean up fetchGraphQL function for improved clarity

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-const API_URI = process.env.API_URL || "http://localhost:3000/api/graphql";
+// const API_URI = process.env.API_URL || "http://localhost:3000/api/graphql";
 
 export const gql = ([content]) => content;
 
 export async function fetchGraphQL(query, variables?, buildQuery = false) {
-  console.log("bq", buildQuery);
-  const apiResponse = await axios.post(API_URI, { query, variables });
-  // if (buildQuery) {
-  //   console.log("Fetching GraphQL with buildQuery enabled:", process.env.API_URL);
-  //   apiResponse = await axios.post(process.env.API_URL!, { query, variables });
-  // } else {
-  //   apiResponse = await axios.post(process.env.NEXT_PUBLIC_API_URL!, { query, variables });
-  // }
+  // console.log("bq", buildQuery);
+  // const apiResponse = await axios.post(API_URI, { query, variables });
+  let apiResponse;
+  if (buildQuery) {
+    console.log("Fetching GraphQL with buildQuery enabled:", process.env.API_URL);
+    apiResponse = await axios.post(process.env.API_URL!, { query, variables });
+  } else {
+    apiResponse = await axios.post(process.env.NEXT_PUBLIC_API_URL!, { query, variables });
+  }
 
   return apiResponse.data.data;
 }
