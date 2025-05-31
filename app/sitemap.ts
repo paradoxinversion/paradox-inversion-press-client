0e278b5a79084984c633fcb8f136a0f57ee29126
Jedai Saboteur <paradox.inversion@gmail.com>
2025-05-31 12:08:59 -0600

    refactor: restructure sitemap function for improved error handling and readability

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,21 +2,27 @@ import { MetadataRoute } from 'next'
 import { formatPostPath, formatSeriesPostPath, getAllPosts, getPages } from 'utils/actions'
  
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const pages = await getPages();
-  const posts = await getAllPosts();
+  try {
 
-  const postsMap = posts.map((post) => {
-    if (post.postType === 'series') {
-      return {
-        url: `${process.env.HOST_URL}${formatSeriesPostPath(post.series.url, post.url)}`,
+    const pages = await getPages();
+    const posts = await getAllPosts();
+  
+    const postsMap = posts.map((post) => {
+      if (post.postType === 'series') {
+        return {
+          url: `${process.env.HOST_URL}${formatSeriesPostPath(post.series.url, post.url)}`,
+        }
       }
-    }
-    return {
-      url: `${process.env.HOST_URL}${formatPostPath(post.publishedAt, post.url)}`,
-    }
-  })
-  const pagesMap = pages.pages.map((page) => ({
-    url: `${process.env.HOST_URL}/page/${page.url}`,
-  }));
-  return [...pagesMap, ...postsMap];
+      return {
+        url: `${process.env.HOST_URL}${formatPostPath(post.publishedAt, post.url)}`,
+      }
+    })
+    const pagesMap = pages.pages.map((page) => ({
+      url: `${process.env.HOST_URL}/page/${page.url}`,
+    }));
+    return [...pagesMap, ...postsMap];
+  } catch (e) {
+    console.error("Error generating sitemap:", e);
+    return [];
+  }
 }
--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.API_URL,
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default axiosInstance;
