ca48d820d9447676f628509bb6df40f74a59dc30
Jedai Saboteur <paradox.inversion@gmail.com>
2025-06-03 15:12:36 -0600

    refactor: update sitemap URLs to use HTTPS and clean up logging in MyApp component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,8 @@ import { getPages } from "utils/actions";
 export default function MyApp({ Component, pageProps }) {
   const [pageData, setPageData] = useState([]);
     useEffect(() => {
-      console.log("Fetching pages data...");
       getPages().then((res) => {
         setPageData(res.pages);
-        console.log("Pages data fetched:", res.pages);
       }).catch((error) => {
         console.error("Error fetching pages:", error);
       });
