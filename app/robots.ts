6023b220d9d7a2eeb9c978b81d5cefe6044de5e4
Jedai Saboteur <paradox.inversion@gmail.com>
2025-06-07 17:28:28 -0600

    refactor: update robots.txt rules to disallow some gen AI scrapers

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,36 @@ import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: 'anthropic-ai',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Bytespider',
+        disallow: '/',
+      },
+      {
+        userAgent: 'Claude-Web',
+        disallow: '/',
+      },
+      {
+        userAgent: 'CCbot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'FacebookBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'PiplBot',
+        disallow: '/',
+      }
+    ],
     sitemap: `${process.env.NEXT_PUBLIC_HOST_URL}/sitemap.xml`,
   }
 }
