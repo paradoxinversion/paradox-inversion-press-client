278feba42d75234c2fe346e963b98dc453012cff
Jedai Saboteur <paradox.inversion@gmail.com>
2026-01-18 14:50:58 -0700

    refactor: allow AI agent crawling

--- app/robots.ts
+++ app/robots.ts
@@ -1,37 +1,37 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [
-      {
-        userAgent: 'anthropic-ai',
-        disallow: '/',
-      },
-      {
-        userAgent: 'Bytespider',
-        disallow: '/',
-      },
-      {
-        userAgent: 'Claude-Web',
-        disallow: '/',
-      },
-      {
-        userAgent: 'CCbot',
-        disallow: '/',
-      },
-      {
-        userAgent: 'FacebookBot',
-        disallow: '/',
-      },
-      {
-        userAgent: 'GPTBot',
-        disallow: '/',
-      },
-      {
-        userAgent: 'PiplBot',
-        disallow: '/',
-      }
-    ],
-    sitemap: `https://${process.env.NEXT_PUBLIC_HOST_URL}/sitemap.xml`,
-  }
+    return {
+        rules: [
+            {
+                userAgent: "anthropic-ai",
+                disallow: "/",
+            },
+            {
+                userAgent: "Bytespider",
+                disallow: "/",
+            },
+            {
+                userAgent: "Claude-Web",
+                disallow: "/",
+            },
+            {
+                userAgent: "CCbot",
+                disallow: "/",
+            },
+            {
+                userAgent: "FacebookBot",
+                disallow: "/",
+            },
+            {
+                userAgent: "GPTBot",
+                disallow: "/",
+            },
+            {
+                userAgent: "PiplBot",
+                disallow: "/",
+            },
+        ],
+        sitemap: `https://${process.env.NEXT_PUBLIC_HOST_URL}/sitemap.xml`,
+    };
 }
